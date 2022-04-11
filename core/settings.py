c48bce58971c6897623a8002c0f618818950db44
luisgs7 <luisgsilva270@gmail.com>
2022-04-11 11:50:07 -0300

    refactor: update static files

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = (os.path.join(BASE_DIR, 'home/templates/static/'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'home/templates/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
